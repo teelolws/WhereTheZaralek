f4452355074fa4220edeb6ce7b50e5ec1ef41200
Teelo <49792789+teelolws@users.noreply.github.com>
2023-05-05 08:11:47 +1200

    Update options.lua

---
+++ Source/options.lua
@@ -0,0 +1,33 @@
+local addonName, addon = ...
+
+EventUtil.ContinueOnAddOnLoaded(addonName, function()
+
+    local LSM = LibStub("LibSharedMedia-3.0")
+    LSM:Register("sound", "Squire Horn", 598079)
+
+    local defaults = {
+        profile = {
+            selectedSound = nil,
+        }
+    }
+        
+    addon.db = LibStub("AceDB-3.0"):New("WhereTheZaralekDB", defaults)
+        
+    local options = {
+        type = "group",
+        args = {
+            selectedSound = {
+                type = "select",
+                dialogControl = "LSM30_Sound",
+                name = "Sound to Play",
+                values = AceGUIWidgetLSMlists.sound,
+                get = function() return addon.db.profile.selectedSound end,
+    		    set = function(info, v) addon.db.profile.selectedSound = v end,
+            },
+        },
+    }
+
+    LibStub("AceConfigRegistry-3.0"):ValidateOptionsTable(options, addonName)
+    LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options, {"wherethezaralek"})
+    LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addonName)
+end)
