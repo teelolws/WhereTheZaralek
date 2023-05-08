7a1469cba230d9ed70aedcb1af76a32c0b67feec
Teelo <49792789+teelolws@users.noreply.github.com>
2023-05-08 15:14:08 +1200

    Add more interface options
    Show completed, show event info, show coordinates, hide specific zones, enable rare support.

---
+++ Source/filter.lua
@@ -0,0 +1,97 @@
+local addonName, addon = ...
+
+local timeBase = 1683298800
+if GetCurrentRegion() == 3 then
+    timeBase = 1683259200
+end
+
+local function formatName(text, completed)
+    if completed then
+        return "|cFF00FF00"..text.."|r"
+    else
+        return text
+    end
+end
+
+local function formatInfo(text)
+    return " "..text
+end
+
+local function formatCoordinates(x, y)
+    return " ("..x..", "..y..")"
+end
+
+function addon:UpdateFilters()
+    local text = "" 
+    local settings = addon.db.global
+
+    local day = time()
+    day = day - timeBase
+    day = day / 86400
+    day = math.floor(day)
+    day = day % 4
+    if day == 0 then day = 4 end
+
+    for groupID = 1, 5 do
+        local include = (groupID ~= day)
+        if (groupID == 1) and settings.hideZoneGlimmerogg then include = false end
+        if (groupID == 2) and settings.hideZoneNal then include = false end
+        if (groupID == 3) and settings.hideZoneLoamm then include = false end
+        if (groupID == 4) and settings.hideZoneAberrus then include = false end
+        if (groupID == 5) and settings.hideZoneCaldera then include = false end
+        
+        if include then
+            for _, event in ipairs(addon.events) do
+                if (event.groupID == groupID) then
+                    local completed = C_QuestLog.IsQuestFlaggedCompleted(event.questID)
+                    if (not completed) or (completed and settings.showCompleted) then
+                        text = text..formatName(event.name, completed)
+                        if settings.showEventInfo and event.info then
+                            text = text..formatInfo(event.info)
+                        end
+                        if settings.showCoordinates then
+                            text = text..formatCoordinates(event.coordX, event.coordY)
+                        end
+                        text = text.."\n"
+                    end
+                end
+            end
+        end
+    end    
+
+    addon:UpdateEventUI(text)
+    
+    text = ""
+    for _, brick in ipairs(addon.bricks) do
+        local completed = C_QuestLog.IsQuestFlaggedCompleted(brick.questID)
+        if (not completed) or (completed and settings.showCompleted) then
+            text = text..formatName(brick.name, completed)
+            if settings.showEventInfo and brick.info then
+                text = text..formatInfo(brick.info)
+            end
+            if settings.showCoordinates then
+                text = text..formatCoordinates(brick.coordX, brick.coordY)
+            end
+            text = text.."\n"
+        end
+    end
+    
+    addon:UpdateBrickUI(text)
+    
+    text = ""
+    for _, rare in ipairs(addon.rares) do
+        local completed = C_QuestLog.IsQuestFlaggedCompleted(rare.questID)
+        if (not completed) or (completed and settings.showCompleted) then
+            text = text..formatName(rare.name, completed)
+            if settings.showEventInfo and rare.info then
+                text = text..formatInfo(rare.info)
+            end
+            if settings.showCoordinates then
+                text = text..formatCoordinates(rare.coordX, rare.coordY)
+            end
+            text = text.."\n"
+        end
+    end
+    
+    addon:UpdateRareUI(text)
+end
--- Source/options.lua
+++ Source/options.lua
@@ -8,6 +8,15 @@ EventUtil.ContinueOnAddOnLoaded(addonName, function()
         global = {
             selectedSound = "Squire Horn",
             flashTaskbar = true,
+            showCompleted = false,
+            showEventInfo = false,
+            showCoordinates = false,
+            hideZoneCaldera = false,
+            hideZoneGlimmerogg = false,
+            hideZoneNal = false,
+            hideZoneLoamm = false,
+            hideZoneAberrus = false,
+            raresEnabled = false,
         }
     }
         
@@ -21,7 +30,7 @@ EventUtil.ContinueOnAddOnLoaded(addonName, function()
                 dialogControl = "LSM30_Sound",
                 name = "Sound to Play",
                 values = AceGUIWidgetLSMlists.sound,
-                get = function() return addon.db.global.selectedSound end,
+                get = function() return addon.db.global.selectedSound  end,
     		    set = function(info, v) addon.db.global.selectedSound = v end,
             },
             flashTaskbar = {
@@ -29,6 +38,60 @@ EventUtil.ContinueOnAddOnLoaded(addonName, function()
                 name = "Flash Taskbar",
                 set = function(info, v) addon.db.global.flashTaskbar = v end,
                 get = function() return addon.db.global.flashTaskbar end,
+            },
+            showCompleted = {
+                type = "toggle",
+                name = "Show Completed",
+                set = function(info, v) addon.db.global.showCompleted = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.showCompleted end,
+            },
+            showEventInfo = {
+                type = "toggle",
+                name = "Show Event Info",
+                set = function(info, v) addon.db.global.showEventInfo = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.showEventInfo end,
+            },
+            showCoordinates = {
+                type = "toggle",
+                name = "Show Coordinates",
+                set = function(info, v) addon.db.global.showCoordinates = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.showCoordinates end,
+            },
+            hideZoneCaldera = {
+                type = "toggle",
+                name = "Hide Zone Caldera",
+                set = function(info, v) addon.db.global.hideZoneCaldera = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.hideZoneCaldera end,
+            },
+            hideZoneGlimmerogg = {
+                type = "toggle",
+                name = "Hide Zone Glimmerogg",
+                set = function(info, v) addon.db.global.hideZoneGlimmerogg = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.hideZoneGlimmerogg end,
+            },
+            hideZoneNal = {
+                type = "toggle",
+                name = "Hide Zone Nal",
+                set = function(info, v) addon.db.global.hideZoneNal = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.hideZoneNal end,
+            },
+            hideZoneLoamm = {
+                type = "toggle",
+                name = "Hide Zone Loamm",
+                set = function(info, v) addon.db.global.hideZoneLoamm = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.hideZoneLoamm end,
+            },
+            hideZoneAberrus = {
+                type = "toggle",
+                name = "Hide Zone Aberrus",
+                set = function(info, v) addon.db.global.hideZoneAberrus = v addon:UpdateFilters() end,
+                get = function() return addon.db.global.hideZoneAberrus end,
+            },
+            raresEnabled = {
+                type = "toggle",
+                name = "Alert for Rares too",
+                set = function(info, v) addon.db.global.raresEnabled = v end,
+                get = function() return addon.db.global.raresEnabled end,
             },
         },
     }
