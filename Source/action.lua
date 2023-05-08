026633224965c07add36e3afd7b0002e4d1a6bcf
Teelo <49792789+teelolws@users.noreply.github.com>
2023-05-08 15:14:25 +1200

    Add rare announce support

--- Source/action.lua
+++ Source/action.lua
@@ -1,19 +1,47 @@
 local addonName, addon = ...
 
+local function setWaypoint(x, y)
+    if C_Map.CanSetUserWaypointOnMap(2133) then
+        local point = UiMapPoint.CreateFromCoordinates(2133, x, y)
+        C_Map.ClearUserWaypoint()
+        C_Map.SetUserWaypoint(point)
+        C_SuperTrack.SetSuperTrackedUserWaypoint(true)
+        
+        addon.playSound()
+        
+        if addon.db.global.flashTaskbar then
+            FlashClientIcon()
+        end
+    end
+end
+
 function addon.action(text)
     for _, data in pairs(addon.events) do
         local announce = data.announce
         if announce and announce[GetLocale()] and (announce[GetLocale()] == text) then
-            if C_Map.CanSetUserWaypointOnMap(2133) then
-                local point = UiMapPoint.CreateFromCoordinates(2133, data.coordX/100, data.coordY/100)
-                C_Map.ClearUserWaypoint()
-                C_Map.SetUserWaypoint(point)
-                C_SuperTrack.SetSuperTrackedUserWaypoint(true)
-                
-                addon.playSound()
-                
-                if addon.db.global.flashTaskbar then
-                    FlashClientIcon()
+            setWaypoint(data.coordX/100, data.coordY/100)
+        end
+    end
+end
+
+local lastAnnounced = {}
+function addon.vignettesUpdated()
+    if not addon.db.global.raresEnabled then return end
+    for _, guid in ipairs(C_VignetteInfo.GetVignettes()) do
+        local info = C_VignetteInfo.GetVignetteInfo(guid)
+        if info and info.objectGUID then
+            local _, _, objectID = info.objectGUID:find("(%d%d%d%d%d%d)")
+            if objectID then
+                objectID = tonumber(objectID)
+                for _, rare in ipairs(addon.rares) do
+                    if rare.objectID == objectID then
+                        if lastAnnounced[rare.questID] and ((lastAnnounced[rare.questID] + 1200) > time()) then
+                        else
+                            addon.playSound()
+                            setWaypoint(rare.coordX/100, rare.coordY/100)
+                            lastAnnounced[rare.questID] = time()
+                        end
+                    end
                 end
             end
         end
--- Source/listener.lua
+++ Source/listener.lua
@@ -2,11 +2,22 @@ local addonName, addon = ...
 
 local listener = CreateFrame("Frame")
 listener:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
+listener:RegisterEvent("PLAYER_ENTERING_WORLD")
+listener:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+listener:RegisterEvent("VIGNETTE_MINIMAP_UPDATED")
+listener:RegisterEvent("VIGNETTES_UPDATED")
 listener:SetScript("OnEvent", function(self, event, text, monsterName)
     if event == "CHAT_MSG_MONSTER_EMOTE" then
         local mapID = C_Map.GetBestMapForUnit("player")
         if mapID ~= 2133 then return end
         
         addon.action(text)
+    elseif (event == "PLAYER_ENTERING_WORLD") or (event == "ZONE_CHANGED_NEW_AREA") or (event == "VIGNETTE_MINIMAP_UPDATED") then
+        local mapID = C_Map.GetBestMapForUnit("player")
+        if mapID ~= 2133 then addon:HideUI() return end
+        
+        addon:UpdateFilters()
+    elseif event == "VIGNETTES_UPDATED" then
+        addon.vignettesUpdated()
     end
 end)
